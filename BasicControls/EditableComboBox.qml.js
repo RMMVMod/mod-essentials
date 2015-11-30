52e502df86868e4ac64b882e1ec5469115379d84
Yeechan Lu <wz.bluesnow@gmail.com>
2015-12-01 02:35:53 +0800

    use remove API

--- BasicControls/EditableComboBox.qml.js
+++ BasicControls/EditableComboBox.qml.js
@@ -5,14 +5,15 @@ var qml = ModAPI.QMLFile("BasicControls/ComboBox.qml")
 var node, objects, obj
 
 node = qml.root.node
-node.object("Keys.onLeftPressed", "{ ; }")
-node.object("Keys.onRightPressed", "{ ; }")
+node.object("Keys.onLeftPressed", null)
+node.object("Keys.onRightPressed", null)
 node.object("editable", "true")
 
 node = qml.getObjectById("mouseArea").node
-node.object("anchors.fill", "20").name = "width"
+node.object("anchors.fill", null)
 node.object("anchors.right", "parent.right")
 node.object("anchors.top", "parent.top")
 node.object("anchors.bottom", "parent.bottom")
+node.object("width", "20")
 
 ModAPI.add("BasicControls/EditableComboBox.qml", qml.code)
